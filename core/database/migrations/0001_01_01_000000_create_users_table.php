71d63897a2f70481b4edc4772ac38b0db05d3763
Administrator <jmlaureano@bapm.com>
2024-09-18 08:26:12 -0400

    Modified seeds

--- core/database/migrations/0001_01_01_000000_create_users_table.php
+++ core/database/migrations/0001_01_01_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -21,12 +22,6 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
@@ -35,6 +30,8 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+
+        Artisan::call('db:seed --class=UserSeeder');
     }
 
     /**
@@ -43,7 +40,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
--- core/database/seeders/DatabaseSeeder.php
+++ core/database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Location;
 use App\Models\Part;
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,16 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Bob Smith',
-            'email' => 'bob@gmail.com',
-        ]);
-
-        User::factory()->create([
-            'name' => 'Tom Wilson',
-            'email' => 'tom@gmail.com',
-        ]);
-
         Part::factory(5)->create();
 
         Location::factory(10)->create();
