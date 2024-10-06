483d174bcd7087bdef7e1cb62d5c3e60982a0477
Justin Laureano <jmlaureano87@gmail.com>
2024-10-06 19:56:05 -0400

    Updating users migration (wip)

--- core/database/migrations/0001_01_01_000000_create_users_table.php
+++ core/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,19 +12,49 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // TODO: columns to create
+        // SELECT
+        // guid,
+        // username,
+        // givenname,
+        // sn,
+        // title,
+        // description,
+        // department,
+        // mail,
+        // manager,
+        // memberof,
+        // password,
+        // remember_token
+        // FROM prospira_web.users;
+        // plus organization_id instead of company
+
+
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->uuid('guid')->primary();
+            $table->string('username')->unique()->nullable();
+            $table->string('company')->nullable();
+            $table->string('department')->nullable();
+            $table->string('description')->nullable();
+            $table->string('displayname')->nullable();
+            $table->string('givenname')->nullable();
+            $table->string('mail')->nullable();
+            $table->string('manager')->nullable();
+            $table->text('memberof')->nullable();
+            $table->string('name')->nullable();
+            $table->string('samaccountname')->nullable();
+            $table->string('sn')->nullable();
+            $table->string('title')->nullable();
+            $table->string('userprincipalname')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->foreignUuId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
