bc21b21650c1f5ac91b410f3c202200e14d8172b
Justin Laureano <jmlaureano87@gmail.com>
2024-10-11 21:37:15 -0400

    Added permission enums

--- core/app/Domain/Auth/Enums/PermissionEnum.php
+++ core/app/Domain/Auth/Enums/PermissionEnum.php
@@ -4,12 +4,17 @@ namespace App\Domain\Auth\Enums;
 
 enum PermissionEnum: string
 {
+    case CREATE_STORAGE_LOCATIONS = 'create storage locations';
     case TRANSFER_MATERIAL_STOCK = 'transfer material stock';
+    case UPDATE_CONTAINER_MOVEMENT_STATUS = 'update container movement status';
 
     public function label() : string
     {
+        // TODO: replace strings with lang strings
         return match ($this) {
+            static::CREATE_STORAGE_LOCATIONS => 'Create Storage Locations',
             static::TRANSFER_MATERIAL_STOCK => 'Transfer Material Stock',
+            static::UPDATE_CONTAINER_MOVEMENT_STATUS => 'Update Container Movement Status',
         };
     }
 }
