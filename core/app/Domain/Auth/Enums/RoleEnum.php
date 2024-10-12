4db9149b608c09559ead80b71f3fb3be4a8092a8
Justin Laureano <jmlaureano87@gmail.com>
2024-10-11 21:39:45 -0400

    Added role enums

--- core/app/Domain/Auth/Enums/RoleEnum.php
+++ core/app/Domain/Auth/Enums/RoleEnum.php
@@ -4,12 +4,14 @@ namespace App\Domain\Auth\Enums;
 
 enum RoleEnum : string
 {
+    case IRM_ADMIN = 'irm-admin';
     case SUPER_ADMIN = 'super-admin';
     case MATERIAL_HANDLER = 'material-handler';
 
     public function label() : string
     {
         return match ($this) {
+            static::IRM_ADMIN => 'IRM Admin',
             static::SUPER_ADMIN => 'Super Admin',
             static::MATERIAL_HANDLER => 'Material Handler',
         };
