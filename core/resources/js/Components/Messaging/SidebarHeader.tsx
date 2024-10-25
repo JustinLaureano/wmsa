95f22776d951fdafb253c72fa76e7c869e0bd4f4
Justin Laureano <jmlaureano87@gmail.com>
2024-10-25 17:38:47 -0400

    Added header

--- core/resources/js/Components/Messaging/SidebarHeader.tsx
+++ core/resources/js/Components/Messaging/SidebarHeader.tsx
@@ -1,4 +1,5 @@
-import { Stack, Typography, useTheme } from '@mui/material';
+import { OpenInNew } from '@mui/icons-material';
+import { Divider, IconButton, Stack, Tooltip, Typography, useTheme } from '@mui/material';
 
 interface SidebarHeaderProps {
 
@@ -8,11 +9,27 @@ export default function SidebarHeader({ ...props }: SidebarHeaderProps ) {
     const theme = useTheme();
 
     return (
-        <Stack
-            direction="row"
-            alignItems="center"
-        >
-            <Typography variant="h6">Messaging</Typography>
-        </Stack>
+        <>
+            <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{
+                    padding: 2
+                }}
+            >
+                <Typography variant="h3">
+                    Messaging
+                </Typography>
+
+                <Tooltip title="New Conversation" arrow>
+                    <IconButton size="large">
+                        <OpenInNew />
+                    </IconButton>
+                </Tooltip>
+            </Stack>
+
+            <Divider />
+        </>
     );
 }
