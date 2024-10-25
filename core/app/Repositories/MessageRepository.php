83f666c33d71d3cb3233b270d5f9affe1dcdc6ed
Justin Laureano <jmlaureano87@gmail.com>
2024-10-24 22:21:54 -0400

    added repository query

--- core/app/Repositories/MessageRepository.php
+++ core/app/Repositories/MessageRepository.php
@@ -4,9 +4,31 @@ namespace App\Repositories;
 
 use App\Domain\Messaging\DataTransferObjects\MessageData;
 use App\Models\Message;
+use Illuminate\Support\Facades\DB;
 
 class MessageRepository
 {
+    /**
+     * Get the number of combined unread messages
+     * for a user across joint accounts.
+     */
+    public function getUnreadMessagesCount(
+        string $primaryId,
+        string $primaryType,
+        string $secondaryId,
+        string $secondaryType
+    ) : int
+    {
+        $result = DB::select('CALL get_unread_messages_count(?, ?, ?, ?)', [
+            $primaryId, 
+            $primaryType, 
+            $secondaryId, 
+            $secondaryType
+        ]);
+
+        return $result[0]->unread_messages ?? 0;
+    }
+
     /**
      * Store a message record.
      */
