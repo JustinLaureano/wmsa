e0f0d5603a6f427fb893cb7395b7ed4ac0b37698
Justin Laureano <jmlaureano87@gmail.com>
2025-01-12 13:37:14 -0500

    Added collection

---
+++ core/app/Http/Resources/Messaging/ParticipantCollection.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Messaging;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ParticipantCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection->toArray(),
+            'computed' => [
+                'count' => $this->collection->count()
+            ],
+            'meta' => [
+                'timestamp' => now()
+            ],
+        ];
+    }
+}
