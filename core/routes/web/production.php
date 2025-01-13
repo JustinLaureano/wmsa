66216418aa4e0295d8707111ab60c680ae4cfdb4
Justin Laureano <jmlaureano87@gmail.com>
2025-01-12 20:56:33 -0500

    fixed middleware

--- core/routes/web/production.php
+++ core/routes/web/production.php
@@ -8,12 +8,6 @@ Route::get('production/requests', ViewProductionRequests::class)
     ->name('production.requests');
 
 Route::middleware('auth')->group(function () {
-    //
-});
-
-Route::middleware('auth:teammate')->group(function () {
-
     Route::get('production/requests/create', CreateRequest::class)
         ->name('production.requests.create');
-
 });
