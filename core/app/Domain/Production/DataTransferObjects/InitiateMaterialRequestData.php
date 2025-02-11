e13d6564abfd6d33a38ad42d7679c14d4950a118
Justin Laureano <jmlaureano87@gmail.com>
2025-02-10 22:46:36 -0500

    refactor

--- core/app/Domain/Production/DataTransferObjects/InitiateMaterialRequestData.php
+++ core/app/Domain/Production/DataTransferObjects/InitiateMaterialRequestData.php
@@ -6,12 +6,11 @@ use Spatie\LaravelData\Data;
 
 class InitiateMaterialRequestData extends Data
 {
+    /**
+     * @param array<InitiateMaterialRequestActionData> $items
+     */
     public function __construct(
-        public readonly string $part_number,
-        public readonly int $quantity,
-        public readonly string $unit_of_measure,
-        public readonly string|null $machine_uuid,
-        public readonly string|null $storage_location_uuid,
+        public readonly array $items,
         public readonly string $requester_user_uuid,
         public readonly string $requested_at,
     )
@@ -22,26 +21,10 @@ class InitiateMaterialRequestData extends Data
     public static function rules(): array
     {
         return [
-            'part_number' => [
+            'items' => [
                 'required',
-                'exists:materials,part_number'
-            ],
-            'quantity' => [
-                'required',
-                'integer'
-            ],
-            'unit_of_measure' => [
-                'required',
-                'string',
+                'array',
                 'min:1'
-            ],
-            'machine_uuid' => [
-                'nullable',
-                'exists:machines,uuid'
-            ],
-            'storage_location_uuid' => [
-                'nullable',
-                'exists:storage_locations,uuid'
             ],
             'requester_user_uuid' => [
                 'required',
--- core/app/Domain/Production/Transformers/MaterialRequestTransformer.php
+++ core/app/Domain/Production/Transformers/MaterialRequestTransformer.php
@@ -6,6 +6,7 @@ use App\Domain\Production\Enums\RequestStatusEnum;
 use App\Domain\Production\DataTransferObjects\MaterialRequestActionData;
 use App\Domain\Production\DataTransferObjects\InitiateMaterialRequestData;
 use App\Domain\Production\Resolvers\RequesterResolver;
+use App\Domain\Production\Transformers\MaterialRequestItemTransformer;
 use Carbon\Carbon;
 use App\Domain\Production\DataTransferObjects\InitiateUpdateMaterialRequestStatusData;
 use App\Domain\Production\DataTransferObjects\UpdateMaterialRequestStatusData;
@@ -19,7 +20,13 @@ class MaterialRequestTransformer
         $requester = RequesterResolver::getRequester($data->requester_user_uuid);
         $requestedAt = new Carbon($data->requested_at);
 
+        $items = [];
+        foreach ($data->items as $item) {
+            $items[] = MaterialRequestItemTransformer::payloadToActionData($item);
+        }
+
         return new MaterialRequestActionData(
+            items: $items,
             material_request_status_code: $statusCode,
             requester: $requester,
             requested_at: $requestedAt
