f95f1cfb7bbf7893d87544f4c3042edae7402c1d
Justin Laureano <jmlaureano87@gmail.com>
2025-05-31 11:52:06 -0400

    Updated types

--- core/resources/js/types/shared/JsonApiResource.types.ts
+++ core/resources/js/types/shared/JsonApiResource.types.ts
@@ -1,6 +1,6 @@
 export interface JsonApiResource<TAttributes, TRelations = Record<string, any>, TComputed = Record<string, any>> {
     uuid: string;
     attributes: TAttributes;
-    relations?: TRelations;
-    computed?: TComputed;
+    relations: TRelations;
+    computed: TComputed;
 }
