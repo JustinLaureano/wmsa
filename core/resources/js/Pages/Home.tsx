ca7771b8bd83f84e488a6102acf4cff3473fad2a
Justin Laureano <jmlaureano87@gmail.com>
2025-06-05 14:53:31 -0400

    refactor

--- core/resources/js/Pages/Home.tsx
+++ core/resources/js/Pages/Home.tsx
@@ -36,20 +36,29 @@ export default function Home({ recentRequests, ...props } : any) {
                 <HomeTabs />
             </Stack>
 
-            <Stack direction="row" spacing={2}>
-                <Skeleton variant="rectangular" width="33%" height={200} />
-                <Skeleton variant="rectangular" width="33%" height={200} />
-                <Skeleton variant="rectangular" width="33%" height={200} />
-            </Stack>
+            <Box
+                sx={{
+                    width: '90%',
+                    mx: 'auto',
+                    maxWidth: 1200,
+                }}
+            >
+                <Stack direction="row" spacing={2}>
+                    <Skeleton variant="rectangular" width="33%" height={200} />
+                    <Skeleton variant="rectangular" width="33%" height={200} />
+                    <Skeleton variant="rectangular" width="33%" height={200} />
+                </Stack>
 
-            <Stack direction="row" spacing={2} sx={{ mt: 4 }}>
-                <Skeleton variant="rectangular" width="50%" height={300} />
-                <Skeleton variant="rectangular" width="50%" height={300} />
-            </Stack>
+                <Stack direction="row" spacing={2} sx={{ mt: 4 }}>
+                    <Skeleton variant="rectangular" width="50%" height={300} />
+                    <Skeleton variant="rectangular" width="50%" height={300} />
+                </Stack>
 
-            <Box sx={{ mt: 4 }}>
-                <Skeleton variant="rectangular" height={300} />
+                <Box sx={{ mt: 4 }}>
+                    <Skeleton variant="rectangular" height={300} />
+                </Box>
             </Box>
+
         </SidebarLayout>
     );
 }
