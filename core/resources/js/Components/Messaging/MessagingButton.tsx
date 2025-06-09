e66b8afb85998118fcb9cb6a0bf631bc537101a6
Justin Laureano <jmlaureano87@gmail.com>
2025-06-09 10:42:47 -0400

    Added unread message badge to app bar

--- core/resources/js/Components/Messaging/MessagingButton.tsx
+++ core/resources/js/Components/Messaging/MessagingButton.tsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import { ChatBubbleOutline, ModeCommentOutlined } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
+import { Badge, IconButton } from '@mui/material';
 import UIContext from '@/Contexts/UIContext';
 import MessagingDialog from './MessagingDialog';
+import MessagingContext from '@/Contexts/MessagingContext';
 
 export default function MessagingButton() {
 	const { messagesDialogOpen, setMessagesDialogOpen } = useContext(UIContext);
+    const { unreadMessages } = useContext(MessagingContext);
 
     const handleButtonClick = (e: React.MouseEvent<HTMLElement>) => {
         setMessagesDialogOpen(true);
@@ -20,6 +22,16 @@ export default function MessagingButton() {
                 onClick={handleButtonClick}
             >
                 <ChatBubbleOutline />
+                <Badge
+                    badgeContent={unreadMessages}
+                    color="primary"
+                    sx={{
+                        '& .MuiBadge-badge': {
+                            top: -8,
+                            right: -1,
+                        },
+                    }}
+                />
             </IconButton>
 
             <MessagingDialog
