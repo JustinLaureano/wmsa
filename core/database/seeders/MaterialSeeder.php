2c2f01c15fe79bd90058e0cbbe7e9e508729693a
Justin Laureano <jmlaureano87@gmail.com>
2025-06-19 12:28:09 -0400

    Added seed logic

--- core/database/seeders/MaterialSeeder.php
+++ core/database/seeders/MaterialSeeder.php
@@ -18,6 +18,8 @@ class MaterialSeeder extends Seeder
     protected array $baseContainerTypes = [];
     protected array $descriptionContainerIds = [];
     protected array $irmChemicalPartNumbers = [];
+    protected array $cementedMetalPartNumbers = [];
+    protected array $materialProperties = [];
 
     /**
      * Run the database seeds.
@@ -27,6 +29,8 @@ class MaterialSeeder extends Seeder
         $this->setMaterialContainerTypes();
         $this->setDescriptionContainerIds();
         $this->setIrmChemicalPartNumbers();
+        $this->setCementedMetalPartNumbers();
+        $this->setMaterialProperties();
         $this->setMaterials();
     }
 
@@ -56,14 +60,13 @@ class MaterialSeeder extends Seeder
         foreach ($csvReader->toArray() as $data) {
             foreach ($data as $key => $row) {
 
-                if (isset($this->baseContainerTypes[$row['part_number']])) {
-                    $materialContainerTypeId = $this->descriptionContainerIds[$this->baseContainerTypes[$row['part_number']]];
-                }
-                else {
-                    $materialContainerTypeId = null;
-                }
-
-                $materialTypeCode = $this->setMaterialTypeCode($row['part_number']);
+                $materialContainerTypeId = $this->getMaterialContainerTypeId($row['part_number']);
+                $materialTypeCode = $this->getMaterialTypeCode($row['part_number']);
+                $expirationDays = $this->getExpirationDays($row['part_number']);
+                $baseContainerUnitQuantity = $this->getBaseContainerUnitQuantity($row['part_number']);
+                $requiredDegasHours = $this->getRequiredDegasHours($row['part_number']);
+                $requiredHoldHours = $this->getRequiredHoldHours($row['part_number']);
+                $servicePart = $this->getServicePart($row['part_number']);
 
                 $materialData = new MaterialData(
                     material_number: $row['material_number'] ? $row['material_number'] : null,
@@ -71,13 +74,13 @@ class MaterialSeeder extends Seeder
                     description: $row['material_description'] ? $row['material_description'] : null,
                     material_type_code: $materialTypeCode,
                     base_quantity: $row['base_quantity'] ? (float) $row['base_quantity'] : null,
-                    base_container_unit_quantity: null,
+                    base_container_unit_quantity: $baseContainerUnitQuantity,
                     base_unit_of_measure: $row['base_unit_of_measure'] ? $row['base_unit_of_measure'] : strtoupper(UnitOfMeasureEnum::EA->value),
-                    expiration_days: null,
-                    required_degas_hours: null,
-                    required_hold_hours: null,
+                    expiration_days: $expirationDays,
+                    required_degas_hours: $requiredDegasHours,
+                    required_hold_hours: $requiredHoldHours,
                     material_container_type_id: $materialContainerTypeId,
-                    service_part: false,
+                    service_part: $servicePart,
                 );
 
                 $data[$key] = array_merge(
@@ -149,17 +152,145 @@ class MaterialSeeder extends Seeder
     }
 
     /**
-     * Set the material type code for a given part number.
+     * Set the cemented metal part numbers.
      */
-    protected function setMaterialTypeCode(string $partNumber) : string|null
+    protected function setCementedMetalPartNumbers() : void
+    {
+        /**
+         * To regenerate csv file from legacy production, use this SQL statement:   
+         * 
+         * SELECT
+         *     *
+         * FROM tblwms_cemented_metal_safety_stock
+         */
+        $file = database_path('data/cemented_metal_safety_stock.csv');
+        $csvReader = new CsvReader($file);
+
+        foreach ($csvReader->toArray() as $data) {
+            foreach ($data as $key => $row) {
+                $this->cementedMetalPartNumbers[$row['item']] = $row['item'];
+            }
+        }
+    }
+
+    /*
+     * Set material properties.
+     */
+    protected function setMaterialProperties() : void
+    {
+        /**
+         * To regenerate csv file from legacy production, use this SQL statement:   
+         * 
+         * SELECT
+         *     *
+         * FROM tblwms_item
+         */
+        $file = database_path('data/tblwms_item.csv');
+        $csvReader = new CsvReader($file);
+
+        foreach ($csvReader->toArray() as $data) {
+            foreach ($data as $key => $row) {
+                $this->materialProperties[$row['item']] = [
+                    'hold_time' => $row['hold_time'],
+                    'degas' => $row['degas'],
+                    'service_part' => $row['service_part'],
+                    'standard_pack_size' => $row['standard_pack_size'],
+                ];
+            }
+        }
+    }
+
+    /**
+     * Get the material container type id for a given part number.
+     */
+    protected function getMaterialContainerTypeId(string $partNumber) : int|null
+    {
+        if ( isset($this->baseContainerTypes[$partNumber]) ) {
+            return $this->descriptionContainerIds[$this->baseContainerTypes[$partNumber]];
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the material type code for a given part number.
+     */
+    protected function getMaterialTypeCode(string $partNumber) : string|null
     {
         if ( isset($this->irmChemicalPartNumbers[$partNumber]) ) {
             return MaterialTypeEnum::IRM->code();
         }
+
+        // General rule of thumb is that compound materials start with a 2.
         if ($partNumber[0] === '2') {
             return MaterialTypeEnum::COMPOUND->code();
         }
 
+        if ( isset($this->cementedMetalPartNumbers[$partNumber]) ) {
+            return MaterialTypeEnum::CEMENTED_METAL->code();
+        }
+
         return null;
+    }
+
+    /**
+     * Get the expiration days for a given part number.
+     */
+    protected function getExpirationDays(string $partNumber) : int|null
+    {
+        // General rule of thumb is that compound materials start with a 2.
+        if ($partNumber[0] === '2') {
+            return 30;
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the base container unit quantity for a given part number.
+     */
+    protected function getBaseContainerUnitQuantity(string $partNumber) : int|null
+    {
+        if ( isset($this->materialProperties[$partNumber]) ) {
+            return (int) $this->materialProperties[$partNumber]['standard_pack_size'];
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the required degas hours for a given part number.
+     */
+    protected function getRequiredDegasHours(string $partNumber) : int|null
+    {
+        if ( isset($this->materialProperties[$partNumber]) ) {
+            return (int) $this->materialProperties[$partNumber]['degas'];
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the required hold hours for a given part number.
+     */
+    protected function getRequiredHoldHours(string $partNumber) : int|null
+    {
+        if ( isset($this->materialProperties[$partNumber]) ) {
+            return (int) $this->materialProperties[$partNumber]['hold_time'];
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the service part for a given part number.
+     */
+    protected function getServicePart(string $partNumber) : bool
+    {
+        if ( isset($this->materialProperties[$partNumber]) ) {
+            return (bool) $this->materialProperties[$partNumber]['service_part'];
+        }
+
+        return false;
     }
 }
