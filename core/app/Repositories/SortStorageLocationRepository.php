74baf432fa3101e7f36ec881689694e16cf0c374
Justin Laureano <jmlaureano87@gmail.com>
2025-07-10 15:33:35 -0400

    Changed return type

--- core/app/Repositories/SortStorageLocationRepository.php
+++ core/app/Repositories/SortStorageLocationRepository.php
@@ -3,22 +3,21 @@
 namespace App\Repositories;
 
 use App\Models\StorageLocation;
-use Illuminate\Support\Collection;
 
 class SortStorageLocationRepository
 {
     /**
      * Get all sort list records.
      */
-    public function getSortStationByBuilding(int $buildingId) : Collection|null
+    public function getSortStationByBuilding(int $buildingId) : StorageLocation|null
     {
         return match ($buildingId) {
             1 => StorageLocation::query()
                 ->where('name', 'Plant 2 Sort')
-                ->get(),
+                ->first(),
             2 => StorageLocation::query()
                 ->where('name', 'Blackhawk Sort')
-                ->get(),
+                ->first(),
             default => null
         };
     }
