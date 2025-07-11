597773184d74acef928a65a526d5400771dcd0f1
Justin Laureano <jmlaureano87@gmail.com>
2025-07-11 12:45:16 -0400

    Added views

---
+++ core/app/Models/Views/ViewUnallocatedMaterialRequestItem.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Views;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ViewUnallocatedMaterialRequestItem extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     */
+    protected $table = 'view_unallocated_material_request_items';
+
+    /**
+     * The primary key for the model.
+     */
+    protected $primaryKey = 'material_request_item_uuid';
+
+    /**
+     * The key type for the model.
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     */
+    public $incrementing = false;
+}
---
+++ core/database/migrations/2025_07_11_163408_create_unallocated_material_request_items_view.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared("
+            CREATE OR REPLACE VIEW `view_unallocated_material_request_items` AS
+                SELECT
+                    mri.uuid AS material_request_item_uuid,
+                    mr.uuid AS material_request_uuid,
+                    m.part_number,
+                    mri.quantity_requested,
+                    mri.quantity_delivered,
+                    mri.unit_of_measure,
+                    mach.name AS machine_name,
+                    sl.name AS storage_location_name,
+                    mtt.tote AS material_tote_type,
+                    mr.material_request_status_code,
+                    mr.material_request_type_code,
+                    mri.request_item_status_code,
+                    t.clock_number AS requester_clock_number,
+                    t.first_name AS requester_first_name,
+                    t.last_name AS requester_last_name,
+                    mri.material_uuid,
+                    mri.machine_uuid,
+                    mri.storage_location_uuid,
+                    mr.requester_user_uuid,
+                    mr.requested_at,
+                    mri.created_at,
+                    mri.updated_at,
+                    mri.deleted_at
+                FROM wms.material_request_items mri
+                LEFT JOIN material_requests mr
+                    ON mr.uuid = mri.material_request_uuid
+                LEFT JOIN request_container_allocations rcl
+                    ON rcl.material_request_item_uuid = mri.uuid
+                LEFT JOIN materials m
+                    ON m.uuid = mri.material_uuid
+                LEFT JOIN users u
+                    ON u.uuid = mr.requester_user_uuid
+                LEFT JOIN machines mach
+                    ON mach.uuid = mri.machine_uuid
+                LEFT JOIN storage_locations sl
+                    ON sl.uuid = mri.storage_location_uuid
+                LEFT JOIN material_tote_types mtt
+                    ON mtt.uuid = mri.material_tote_type_uuid
+                LEFT JOIN teammates t
+                    ON t.clock_number = u.teammate_clock_number
+                WHERE mri.request_item_status_code = 'open'
+                    AND mr.material_request_status_code = 'open'
+                    AND rcl.uuid IS NULL
+                ORDER BY mri.created_at ASC;
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared("DROP VIEW IF EXISTS view_unallocated_material_request_items");
+    }
+};
